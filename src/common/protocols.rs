d8e3352fdb617ad4a8487a4777487e07d64d8853
Ion Banaru <ibanaru@devolutions.net>
2018-05-11 15:47:18 -0400

    siquery: fix invalid etc/protocols entry

--- src/common/protocols.rs
+++ src/common/protocols.rs
@@ -14,8 +14,8 @@ cfg_if! {
 lazy_static! {
         //regex filter to extract protocols groups: remove everything from "#" till the line break
         static ref PROTOCOLS_FILE_REGEX: Regex = Regex::new(r"(?m)^([^#]*)").unwrap();
-        //regex filter to extract relevant comments: keep only what follows a "#" on the lines that were previously matched
-        static ref PROTOCOLS_FILE_REGEX_COMMENTS: Regex = Regex::new(r"(?m)^[^#].*#\s*(.*)").unwrap();
+        //regex filter to extract comments: keep everything following a "#"
+        static ref PROTOCOLS_FILE_REGEX_COMMENTS: Regex = Regex::new(r"#\s*(.*)").unwrap();
     }
 
 impl EtcProtocols {
@@ -35,35 +35,50 @@ impl EtcProtocols {
 
         for line in system_reader.get_protocols_file().unwrap_or("".to_string()).lines() {
             let mut etc_protocols = EtcProtocols::new();
+
             let captures = PROTOCOLS_FILE_REGEX.captures(&line);
             if let Some(cap) = captures {
                 if let Some(protocols_group) = cap.get(0) {
+
                     //omitting empty outputs from regex
                     if protocols_group.as_str().len() == 0 {
                         continue
                     }
+
                     let v: Vec<_> = protocols_group.as_str().trim().split_whitespace().collect();
-                    //the ip address will always be the leftmost entry
-                    etc_protocols.name = v[0].to_string();
-                    etc_protocols.number = v[1].to_string();
-                    if v[2].to_string().len() != 0 {
-                        etc_protocols.alias = v[2].to_string();
-                    };
-                };
-            }
-            let comments = PROTOCOLS_FILE_REGEX_COMMENTS.captures(&line);
-            if let Some(cap) = comments {
-                if let Some(cap_comments) = cap.get(1) {
-                    //omitting empty outputs from regex
-                    if cap_comments.as_str().len() == 0 {
+
+                    //check entry for validity
+                    //<protocol name>  <assigned number>  [aliases...]   [#<comment>]
+                    if v.len() <= 2 {
                         continue
                     }
-                    etc_protocols.comment = cap_comments.as_str().to_owned();
+
+                    etc_protocols.name = v[0].to_string();
+                    etc_protocols.number = v[1].to_string();
+
+                    //get alias if it exists
+                    if let Some(_t) = v.get(2) {
+                        etc_protocols.alias = v[2].to_string();
+                    }
                 };
-            }
+
+                //nested to match only relevant comments
+                let comments = PROTOCOLS_FILE_REGEX_COMMENTS.captures(&line);
+                if let Some(cap) = comments {
+                    if let Some(captured_comments) = cap.get(1) {
+                        //omitting empty outputs from regex
+                        if captured_comments.as_str().len() == 0 {
+                            continue
+                        }
+
+                        etc_protocols.comment = captured_comments.as_str().to_owned();
+                    };
+                }
+            };
             protocols.push(etc_protocols);
         }
         protocols
     }
 }
+
 
