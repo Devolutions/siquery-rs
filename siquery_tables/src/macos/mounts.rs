e0a674bd3dbe9a91cb9731e0868ac508d90f97e9
siquery <siquery@wayk-macbook.local>
2018-09-17 11:24:53 -0400

    fix mounts for macos

--- siquery_tables/src/macos/mounts.rs
+++ siquery_tables/src/macos/mounts.rs
@@ -1,88 +1,17 @@
 use std::ffi::CStr;
-use libc::{ c_int,c_char,uid_t,int32_t};
+use libc::{ c_int,statfs };
 use std::{ ptr,slice };
+
 use tables::MountsRow;
 
 extern "C" {
+    #[cfg_attr(target_os = "macos", link_name = "getmntinfo$INODE64")]
     pub fn getmntinfo (mntbufp: &*mut statfs, flags: c_int) -> c_int;
-}
-
-#[repr(C)]
-struct fsid_t {
-    val: [int32_t; 2],
-}
-
-cfg_if! {
-    // Since Mac OSX 10.5 the struct statfs was updated to accommodate for wider f_fstypename,
-    // f_mntonname, and f_mntfromname fields. To ensure reliable output interpretation and proper
-    // allignment, the statfs struct should be defined differently when the
-    // _DARWIN_FEATURE_64_BIT_INODE macro is defined.
-    // https://www.unix.com/man-page/osx/2/stat/
-
-    if #[cfg(not(env = "darwin_feature_64_bit_inode"))] {
-        use libc::{c_short,c_long};
-
-        const MFSNAMELEN: usize = 15;
-        const MNAMELEN: usize = 90;
-
-        #[repr(C)]
-        // Use legacy statfs for 32 bit inodes.
-        pub struct statfs {
-            f_otype: c_short,
-            f_oflags: c_short,
-            f_bsize: c_long,
-            f_iosize: c_long,
-            f_blocks: c_long,
-            f_bfree: c_long,
-            f_bavail: c_long,
-            f_files: c_long,
-            f_ffree: c_long,
-            f_fsid: fsid_t,
-            f_owner: uid_t,
-            f_reserved1: c_short,
-            f_type: c_short,
-            f_flags: c_long,
-            f_reserved2: [c_long;2],
-            f_fstypename: [c_char;MFSNAMELEN],
-            f_mntonname: [c_char;MNAMELEN],
-            f_mntfromname: [c_char;MNAMELEN],
-            f_reserved3: c_char,
-            f_reserved4: [c_long;4],
-        }
-    }
-    else if #[cfg(env = "darwin_feature_64_bit_inode")] {
-        use libc::{uint32_t,uint64_t};
-
-        const MFSTYPENAMELEN : usize = 16;
-        const MAXPATHLEN : usize = 1024;
-
-        #[repr(C)]
-        // Use new statfs for 64 bit inodes.
-        pub struct statfs {
-            f_bsize: uint32_t,
-            f_iosize: int32_t,
-            f_blocks: uint64_t,
-            f_bfree: uint64_t,
-            f_bavail: uint64_t,
-            f_files: uint64_t,
-            f_ffree: uint64_t,
-            f_fsid:	fsid_t,
-            f_owner: uid_t,
-            f_type:	uint32_t,
-            f_flags: uint32_t,
-            f_fssubtype: uint32_t,
-            f_fstypename: [c_char;MFSTYPENAMELEN],
-            f_mntonname: [c_char;MAXPATHLEN],
-            f_mntfromname: [c_char;MAXPATHLEN],
-            f_reserved: [uint32_t;8],
-        }
-    }
 }
 
 static MNT_WAIT : i32 = 1;
 
 impl MountsRow {
-
     pub fn get_specific() -> Vec<MountsRow> {
         let mptr: *mut statfs = ptr::null_mut();
         let len = unsafe { getmntinfo(&mptr,MNT_WAIT) } as usize;
