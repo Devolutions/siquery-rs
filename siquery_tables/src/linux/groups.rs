1b818b68fc0f8bf215a87f5a3c10328f07cff537
Ion Banaru <banaruion@yahoo.ca>
2018-10-11 01:07:28 -0400

    cleanup

--- siquery_tables/src/linux/groups.rs
+++ siquery_tables/src/linux/groups.rs
@@ -19,13 +19,6 @@ impl GroupsRow {
         let mut hash_set = HashSet::new();
         unsafe {setgrent()};
         let mut group_p: *mut group = unsafe {getgrent()};
-        /*The getgrent() function searches all available directory services on it's
-        first invocation.  It caches the returned entries in a list and returns
-        group entries one at a time.
-        NOTE that getgrent() may cause a very lengthy search for group records by
-        opendirectoryd and may result in a large number of group records being
-        cached by the calling process.  Use of this function is not advised.*/
-        //TODO: store all groupnames in a set and use getgrnam(groupname) instead.
         while group_p != ptr::null_mut() {
             let groupname = unsafe{CStr::from_ptr(unsafe {*group_p}.gr_name).to_str().unwrap_or("")};
             if !hash_set.contains(groupname) {
