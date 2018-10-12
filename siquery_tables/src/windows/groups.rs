3c3c967dfc6400eba8489f94e02a2e2bab7fe17a
Ion <banaruion@yahoo.ca>
2018-10-12 12:54:37 -0400

    fix get_sid_from_username() output lifetime for windows

--- siquery_tables/src/windows/groups.rs
+++ siquery_tables/src/windows/groups.rs
@@ -29,6 +29,7 @@ use winapi::{
         },
         winnt::{
             PSID,
+            SID,
             SID_NAME_USE,
             PSID_NAME_USE,
             SidTypeUnknown
@@ -55,7 +56,7 @@ use windows::processes;
 #[allow(non_upper_case_globals)]
 static NERR_Success: DWORD = 0;
 
-pub fn get_sid_from_username(account_name: LPCWSTR) -> PSID {
+pub fn get_sid_from_username(account_name: LPCWSTR) -> Result<SID, DWORD> {
     //? if account_name is empty print "No account name provided"
     // return ptr_null_mut();
 
@@ -81,8 +82,8 @@ pub fn get_sid_from_username(account_name: LPCWSTR) -> PSID {
     };
 
     if ret == 0 && unsafe {GetLastError()} != ERROR_INSUFFICIENT_BUFFER {
-        println!("failed to lookup account name with error {}",/*account_name.to_string(),*/ unsafe {GetLastError()});
-        return ptr::null_mut();
+        println!("failed to lookup account name",/*account_name.to_string(),*/);
+        return Err(unsafe{GetLastError()});
     };
 
     // Buffers.
@@ -105,12 +106,13 @@ pub fn get_sid_from_username(account_name: LPCWSTR) -> PSID {
 
     if ret == 0 {
         println!("failed to lookup account name with error {}", /*account_name.to_string(),*/ unsafe { GetLastError() });
-        return ptr::null_mut();
+        return Err(unsafe{GetLastError()});
     } else if unsafe { IsValidSid(sid_buf_p)} == FALSE {
         println!("The sid for is invalid"/*, account_name.to_string()*/);
-        return ptr::null_mut();
+        return Err(unsafe{GetLastError()});
     }
-    sid_buf_p
+    let sid_p = sid_buf_p as *mut _ as *mut SID;
+    Ok(unsafe{*sid_p})
 }
 
 pub fn get_rid_from_sid(sid_p: PSID) -> i64 {
@@ -174,41 +176,42 @@ impl GroupsRow {
             let lgrpi1_name = local_group_info_1.lgrpi1_name;
             let lgrpi1_comment = local_group_info_1.lgrpi1_comment;
 
-            let sid_p: PSID = get_sid_from_username(lgrpi1_name);
+            if let Ok(mut sid) = get_sid_from_username(lgrpi1_name){
+                let sid_p = &mut sid as *mut _ as PSID;
+                if sid_p != ptr::null_mut(){    //TODO check condition for accuracy.
+                    // Read from buffers.
+                    let groupname = unsafe {
+                        WideString::from_ptr(
+                            lgrpi1_name,
+                            libc::wcslen(lgrpi1_name)
+                        )
+                    };
+                    let comment = unsafe {
+                        WideString::from_ptr(
+                            lgrpi1_comment,
+                            libc::wcslen(lgrpi1_comment)
+                        )
+                    };
 
-            if sid_p != ptr::null_mut(){
-                // Read from buffers.
-                let groupname = unsafe {
-                    WideString::from_ptr(
-                        lgrpi1_name,
-                        libc::wcslen(lgrpi1_name)
-                    )
-                };
-                let comment = unsafe {
-                    WideString::from_ptr(
-                        lgrpi1_comment,
-                        libc::wcslen(lgrpi1_comment)
-                    )
-                };
-
-                out.push(
-                    GroupsRow{
-                        gid: get_rid_from_sid(sid_p),
-                        gid_signed: get_rid_from_sid(sid_p),
-                        groupname: groupname.to_string().unwrap_or("".to_string()),
-                        group_sid: processes::sid_to_string(sid_p).unwrap_or("".to_string()),
-                        comment: comment.to_string().unwrap_or("".to_string())
-                    }
-                );
-            } else {
-                let groupname = unsafe {
-                    WideString::from_ptr(
-                        lgrpi1_name,
-                        libc::wcslen(lgrpi1_name)
-                    )
-                };
-                print!("Failed to find sid from LookupAccountNameW for group: {:?}", groupname);
-            }
+                    out.push(
+                        GroupsRow{
+                            gid: get_rid_from_sid(sid_p),
+                            gid_signed: get_rid_from_sid(sid_p),
+                            groupname: groupname.to_string().unwrap_or("".to_string()),
+                            group_sid: processes::sid_to_string(sid_p).unwrap_or("error".to_string()),
+                            comment: comment.to_string().unwrap_or("".to_string())
+                        }
+                    );
+                } else {
+                    let groupname = unsafe {
+                        WideString::from_ptr(
+                            lgrpi1_name,
+                            libc::wcslen(lgrpi1_name)
+                        )
+                    };
+                    print!("Failed to find sid from LookupAccountNameW for group: {:?}", groupname);
+                }
+            };
         }
         out
     }
