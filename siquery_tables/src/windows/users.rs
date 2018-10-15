e36e62f8d3f8ca8ccdd32fb0c6bad234a7d1cba7
SalahKmt <mohamed-salah-eddine.kamate@polymtl.ca>
2018-10-14 21:37:01 -0400

    users table done & code cleaning

--- siquery_tables/src/windows/users.rs
+++ siquery_tables/src/windows/users.rs
@@ -1,56 +1,29 @@
 use tables::Users;
 use winreg::RegKey;
 use winreg::enums::*;
-use winapi::um::winnt::LPSTR;
-use winapi::ctypes::*;
-use std::ffi::CStr;
-use std::ptr;
-use winapi::shared::lmcons::MAX_PREFERRED_LENGTH;
-use winapi::shared::minwindef::LPBYTE;
-use winapi::shared::minwindef::LPDWORD;
-use winapi::shared::ntdef::LPCWSTR;
-use winapi::shared::winerror::*;
-use std::mem;
-use winapi::um::lmaccess::NetUserEnum;
-use winapi::um::lmaccess::LPUSER_INFO_3;
-use winapi::um::lmaccess::LPUSER_INFO_4;
-use winapi::um::lmaccess::NetUserGetInfo;
-use winapi::um::lmaccess::USER_INFO_4;
-use winapi::um::lmaccess::USER_INFO_3;
-use winapi::um::lmaccess::PUSER_INFO_4;
-use winapi::um::lmaccess::PUSER_INFO_3;
-use winapi::um::lmapibuf::NetApiBufferFree;
-use winapi::shared::winerror::*;
-use winapi::ctypes::wchar_t;
-use winapi::shared::minwindef::BOOL;
-use std::i64::MAX;
-
-use winapi::{
-    um::{
-        winnt::{PSID,PSID_NAME_USE},
-        errhandlingapi::GetLastError,
-        winbase::LocalFree,
-        winbase::LocalReAlloc
-    },
-    shared::{
-        minwindef::{
-            DWORD,
-            HLOCAL
-        },
-        ntdef::{
-            LPWSTR,
-            LPCSTR,
-            NULL
-        },
-        sddl::ConvertSidToStringSidW
-    }
-};
+use std::{ptr, mem, i64::MAX};
 use widestring::WideString;
 use libc;
+use winapi::{
+    ctypes::*,
+    um::{
+        winnt::{PSID, PSID_NAME_USE, SID_NAME_USE, SidTypeUnknown},
+        lmaccess::{NetUserEnum, NetUserGetInfo, LPUSER_INFO_3, LPUSER_INFO_4, USER_INFO_4},
+        lmapibuf::NetApiBufferFree,
+        errhandlingapi::GetLastError
+    },
+    shared::{
+        minwindef::{DWORD, LPDWORD, BOOL},
+        ntdef::{LPWSTR, LPCWSTR, NULL},
+        sddl::{ConvertSidToStringSidW, ConvertStringSidToSidW},
+        lmcons::{MAX_PREFERRED_LENGTH, UNLEN, DNLEN},
+        winerror::*,
+    }
+};
 
-extern "C" {
-    pub fn LookupAccountSidW (
-        lpSystemName: LPCSTR,
+extern "system" {
+    fn LookupAccountSidW(
+        lpSystemName: LPCWSTR,
         Sid: PSID,
         Name: LPWSTR,
         cchName: LPDWORD,
@@ -60,8 +33,8 @@ extern "C" {
     ) -> BOOL;
 }
 
-const NERR_Success: u32 = 0;
-const kWellKnownSids: [&'static str; 17 ] = [
+const NERR_SUCCESS: u32 = 0;
+const K_WELL_KNOWN_SIDS: [&'static str; 17 ] = [
     "S-1-5-1",
     "S-1-5-2",
     "S-1-5-3",
@@ -106,22 +79,20 @@ impl Users {
 }
 
 fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String>) {
-    let mut dw_user_info_level: c_ulong = 3;
+    let dw_user_info_level: c_ulong = 3;
 
     let mut dw_num_users_read_int = 0u32;
-    let mut dw_num_users_read: *mut c_ulong = &mut dw_num_users_read_int as *mut c_ulong;
+    let dw_num_users_read: *mut c_ulong = &mut dw_num_users_read_int as *mut c_ulong;
 
     let mut dw_total_users_int = 0u32;
-    let mut dw_total_users: *mut c_ulong = &mut dw_total_users_int as *mut c_ulong;
+    let dw_total_users: *mut c_ulong = &mut dw_total_users_int as *mut c_ulong;
 
     let mut resume_handle_int = 0u32;
-    let mut resume_handle: *mut c_ulong = &mut resume_handle_int as *mut c_ulong;
-
-    let mut ret: u32 = 0;
+    let resume_handle: *mut c_ulong = &mut resume_handle_int as *mut c_ulong;
 
     let mut user_buffer: Vec<*mut u8> = Vec::with_capacity((MAX_PREFERRED_LENGTH) as usize);
     loop {
-        ret = unsafe {
+        let mut ret = unsafe {
             NetUserEnum(ptr::null(),
                         dw_user_info_level,
                         0 as DWORD,
@@ -132,12 +103,12 @@ fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String
                         resume_handle)
         };
 
-        if (ret == NERR_Success || ret == ERROR_MORE_DATA) &&
+        if (ret == NERR_SUCCESS || ret == ERROR_MORE_DATA) &&
             user_buffer.as_mut_ptr() != ptr::null_mut() {
 
             let mut iter_buff: LPUSER_INFO_3 = unsafe { ptr::read(user_buffer.as_mut_ptr() as *mut _) };
 
-            for i in 0..unsafe { *dw_num_users_read }  {
+            for _i in 0..unsafe { *dw_num_users_read }  {
                 let mut user = Users::new();
                 let mut dw_detailed_user_info_level: c_ulong = 4;
                 let mut user_lvl_4buff: Vec<*mut u8> = Vec::with_capacity((mem::size_of::<USER_INFO_4>()) as usize);
@@ -149,7 +120,7 @@ fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String
                                    user_lvl_4buff.as_mut_ptr())
                 };
 
-                if ret != NERR_Success || user_lvl_4buff.as_mut_ptr() == ptr::null_mut() {
+                if ret != NERR_SUCCESS || user_lvl_4buff.as_mut_ptr() == ptr::null_mut() {
                     unsafe {
                         if user_lvl_4buff.as_mut_ptr() != ptr::null_mut() {
                             NetApiBufferFree(*user_lvl_4buff.as_mut_ptr() as *mut c_void);
@@ -160,11 +131,9 @@ fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String
                     }
                     continue;
                 }
-
                 // Will return empty string on fail
                 let mut lp_user_info_4: LPUSER_INFO_4 = unsafe { ptr::read(user_lvl_4buff.as_mut_ptr() as _) };
                 let mut sid: *mut c_void = unsafe { (*lp_user_info_4).usri4_user_sid };
-
                 unsafe {
                     if let Ok(username) = lpwstr_to_string((*iter_buff).usri3_name) {
                         user.username = username;
@@ -173,7 +142,6 @@ fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String
                         user.description = description;
                     }
                 }
-
                 user.shell = "C:\\Windows\\System32\\cmd.exe".to_string();
                 user.type_ = "local".to_string();
                 if let Ok(sid_string) = sid_to_string(sid) {
@@ -211,11 +179,9 @@ fn process_local_accounts(users: &mut Vec<Users>, processed_sid: &mut Vec<String
     }
 }
 
-//todo
 fn process_roaming_profiles(users: &mut Vec<Users>, processed_sid: &mut Vec<String>){
     let key = r#"Software\Microsoft\Windows NT\CurrentVersion\ProfileList"#;
     let hklm = &RegKey::predef(HKEY_LOCAL_MACHINE);
-    let processed_sid_iter = processed_sid.iter();
 
     if let Ok(profile) = hklm.open_subkey_with_flags(key, KEY_READ) {
         let mut type_ : String = "".to_string();
@@ -244,7 +210,7 @@ fn process_roaming_profiles(users: &mut Vec<Users>, processed_sid: &mut Vec<Stri
                     user.uid_signed = user.uid;
                     user.gid_signed = user.gid;
                     let mut known_sid = false;
-                    for sid in kWellKnownSids.iter(){
+                    for sid in K_WELL_KNOWN_SIDS.iter(){
                         if *sid == sid_string.as_str() {
                             user.type_ = "special".to_string();
                             known_sid = true;
@@ -253,6 +219,8 @@ fn process_roaming_profiles(users: &mut Vec<Users>, processed_sid: &mut Vec<Stri
                     if !known_sid {
                         user.type_ = "roaming".to_string();
                     }
+                    let mut sid: *mut PSID = convert_string_sid_to_sid(sid_string.clone());
+                    user.username = get_roaming_profiles_username(sid);
                     user.shell = "C:\\Windows\\system32\\cmd.exe".to_string();
                     user.description = "".to_string();
                     users.push(user);
@@ -274,13 +242,6 @@ fn get_user_home_dir(sid_string: String)->String {
     home_dir
 }
 
-fn from_wide_string(s: &[u16]) -> String {
-    use std::ffi::OsString;
-    use std::os::windows::ffi::OsStringExt;
-    let slice = s.split(|&v| v == 0).next().unwrap();
-    OsString::from_wide(slice).to_string_lossy().into()
-}
-
 fn get_user_description (lpwstr: LPWSTR) -> Result<String, DWORD> {
     unsafe {
         let buf_size = libc::wcslen(lpwstr);
@@ -291,11 +252,13 @@ fn get_user_description (lpwstr: LPWSTR) -> Result<String, DWORD> {
 
 pub fn sid_to_string(sid: PSID) -> Result<String, DWORD> {
     let mut buf: LPWSTR = NULL as LPWSTR;
-    if unsafe { ConvertSidToStringSidW(sid, &mut buf) } == 0 ||
-        buf == (NULL as LPWSTR) {
-        return Err(unsafe { GetLastError() });
+    unsafe {
+        if ConvertSidToStringSidW(sid, &mut buf) == 0 ||
+            buf == (NULL as LPWSTR) {
+            return Err(GetLastError());
+        }
+        lpwstr_to_string(buf)
     }
-    lpwstr_to_string(buf)
 }
 
 pub fn lpwstr_to_string(lpwstr: LPWSTR) -> Result<String, DWORD> {
@@ -313,27 +276,57 @@ pub fn get_uid_from_sid (sid_string: String) -> i64 {
     return uid
 }
 
-pub fn lookup_account_sid_internal (
-    lp_system_name: LPCSTR,
-    sid: PSID,
-    name: LPWSTR,
-    cch_name: LPDWORD,
-    referenced_domain_name: LPWSTR,
-    cch_referenced_domain_name: LPDWORD,
-    pe_use: PSID_NAME_USE
-) -> BOOL {
+pub fn convert_string_sid_to_sid(sid_string: String) -> *mut PSID {
+    let mut sid_c_str: Vec<u16> = sid_string.clone().as_str().encode_utf16().collect();
+    sid_c_str.push(0);
+
+    let mut sid_buf: Vec<u16> = Vec::with_capacity(sid_c_str.len() as usize);
+    let sid: *mut PSID = sid_buf.as_mut_ptr() as *mut PSID;
     unsafe {
-        LookupAccountSidW (
-            lp_system_name,
-            sid,
-            name,
-            cch_name,
-            referenced_domain_name,
-            cch_referenced_domain_name,
-            pe_use
-        )
+        let ret = ConvertStringSidToSidW(sid_c_str.as_ptr(), sid);
+        if ret == 0 && GetLastError() != ERROR_INSUFFICIENT_BUFFER {
+            println!("failed with error {:?}", ret);
+        };
     }
+    sid
 }
 
-//todo
-pub fn get_roaming_profiles_user_name() {}
+pub fn get_roaming_profiles_username(sid: *mut PSID) -> String {
+    let mut dom_name_len = DNLEN;
+    let dom_name_len_p: LPDWORD = &mut dom_name_len as LPDWORD;
+    let mut dom_name: [wchar_t; DNLEN as usize ] = [0; DNLEN as usize];
+
+    let mut account_name_len = UNLEN;
+    let account_name_len_p: LPDWORD = &mut account_name_len as LPDWORD;
+    let mut account_name: [wchar_t; UNLEN as usize] = [0; UNLEN as usize];
+
+    let mut e_sid_type: SID_NAME_USE = SidTypeUnknown as SID_NAME_USE;
+    let e_sid_type_p: PSID_NAME_USE = &mut e_sid_type as PSID_NAME_USE;
+
+    let ret: BOOL =  unsafe {
+        LookupAccountSidW(
+            ptr::null_mut(),
+            *sid,
+            account_name.as_mut_ptr(),
+            account_name_len_p,
+            dom_name.as_mut_ptr(),
+            dom_name_len_p,
+            e_sid_type_p
+        )
+    };
+
+    if ret == 0 && unsafe {GetLastError()} != ERROR_INSUFFICIENT_BUFFER {
+        println!("failed to lookup account name");
+        return "".to_string()
+    };
+
+    let mut acc_name = String::from_utf16_lossy(&account_name).to_string();
+
+    while acc_name.ends_with("\u{0}") {
+        let len = acc_name.len();
+        let new_len = len.saturating_sub("\u{0}".len());
+        acc_name.truncate(new_len);
+    }
+
+    acc_name
+}
