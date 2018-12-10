323a9acabebfd38761b3599d8ff611e3f156dbd4
Ion Banaru <banaruion@yahoo.ca>
2018-12-10 14:33:21 -0500

    add preferences.plist reader interface for macos

--- siquery_tables/src/macos/proxies.rs
+++ siquery_tables/src/macos/proxies.rs
@@ -1,11 +1,14 @@
-use proxy_config::macos::{get_proxy_config_ex};
+use proxy_config::macos::{get_proxy_config_ex,Reader,ProxyConfigReader};
+use std::borrow::Borrow;
 
 use tables::ProxiesRow;
 
 impl ProxiesRow {
     pub fn get_specific() -> Vec<ProxiesRow> {
         let mut out = Vec::new();
-        let proxies = get_proxy_config_ex().ok().unwrap_or(Vec::new());
+        let reader: Box<ProxyConfigReader> = Box::new(Reader{});
+
+        let proxies = get_proxy_config_ex(reader.borrow()).ok().unwrap_or(Vec::new());
         for entry in proxies {
             out.push(
                 ProxiesRow{
