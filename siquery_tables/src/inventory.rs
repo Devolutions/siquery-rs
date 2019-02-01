e238af97d3644cde1b375b2caa0cfbae444a7f8c
SalahKmt <mohamed-salah-eddine.kamate@polymtl.ca>
2019-02-01 13:12:14 -0500

    cleaning branch

--- siquery_tables/src/inventory.rs
+++ siquery_tables/src/inventory.rs
@@ -2,113 +2,6 @@ use treexml::{Element,Document,XmlVersion::Version10};
 use heck::CamelCase;
 use tables::*;
 
-/*pub fn print_xml () {
-    // RDM Structures.
-    let mut root = Element::new("InventorySystemInformation");
-    /*let mut bios = bios();
-    let mut cd_roms = cd_roms();
-    let mut devices = devices();
-    let mut ip_address = ip_address();
-    let mut local_accounts = local_accounts();
-    let mut logical_drives = logical_drives();
-    let mut memory = memory();
-    let mut monitors = monitors();
-    let mut motherobaords = motherboards();
-    let mut network_adapters = network_adapters();*/
-
-
-
-
-
-
-
-
-    let mut operating_system = Element::new("OperatingSystem");
-
-    let mut printers = Element::new("Printers");
-    let mut remote_printer = Element::new("RemotePrinter");
-
-    let mut processors = Element::new("Processors");
-    let mut remote_processors = Element::new("RemoteProcessors");
-
-    let mut products = Element::new("Products");
-    let mut remote_program = Element::new("RemoteProgram");
-
-    let mut quick_fix_engineerings = Element::new("QuickFixEngineerings");
-    let mut remote_quick_fix_engineering = Element::new("RemoteQuickFixEngineering");
-
-    let mut services = Element::new("Services");
-    let mut remote_service = Element::new("RemoteService");
-
-    let mut shares = Element::new("Shares");
-    let mut remote_share = Element::new("RemoteShare");
-
-    let mut sound_devices = Element::new("SoundDevice");
-    let mut remote_sound = Element::new("RemoteSound");
-
-    let mut start_ups = Element::new("StartUps");
-    let mut remote_start_up = Element::new("RemoteStartUp");
-
-    let mut system = Element::new("System");
-
-    let mut time_zone = Element::new("TimeZone");
-    let mut description = Element::new("Description");
-
-    let mut videos = Element::new("Videos");
-    let mut remote_video = Element::new("RemoteVideos");
-
-
-
-    root.children.push(local_accounts);
-    root.children.push(bios);
-    root.children.push(cd_roms);
-    root.children.push(devices);
-    root.children.push(ip_address);
-    //root.children.push(local_accounts);
-    root.children.push(logical_drives);
-    root.children.push(memory);
-    root.children.push(monitors);
-    root.children.push(motherobaords);
-    root.children.push(network_adapters);
-
-    let doc = Document {
-        root: Some(root),
-        version: Version10,
-        .. Document::default()
-    };
-
-    println!("{}",doc.to_string());
-}*/
-
-/*fn bios() -> Element {
-    let bios = WmiBios::get_specific();
-
-    let mut parent = Element::new("Bios");
-
-    for entry in bios {
-        let mut child_1 = Element::new("Caption");
-        let mut child_2 = Element::new("Manufacturer");
-        let mut child_3 = Element::new("ReleaseDate");
-        let mut child_4 = Element::new("SerialNumber");
-        let mut child_5 = Element::new("SMBIOSBIOSVersion");
-
-        child_1.text = Some(entry.caption);
-        child_2.text = Some(entry.manufacturer);
-        child_3.text = Some(entry.release_date);
-        child_4.text = Some(entry.serial_number);
-        child_5.text = Some(entry.smbios_version);
-
-        parent.children.push(child_1);
-        parent.children.push(child_2);
-        parent.children.push(child_3);
-        parent.children.push(child_4);
-        parent.children.push(child_5);
-    }
-
-    parent
-}*/
-
-
 pub fn get_local_accounts_inv() /*-> String*/ {
     #[cfg(feature = "wmi_local_accounts")]
     let table = WmiLocalAccounts::get_specific();
@@ -204,56 +97,56 @@ pub fn get_hotfixes_inv() /*-> String*/ {
     let table = WmiHotfixes::get_specific();
 }
 
-pub fn execute_inventory_query(query: &str) -> Vec<String> {
+pub fn execute_inventory_query(query: &str) {
     let mut rdm_inv_queries: Vec<String> = Vec::new();
     let query_string = query.to_string();
 
     let local_accounts = "Local Accounts";
     let local_accounts_idx = query_string.find("Local Accounts");
     if let Some(_i) = local_accounts_idx {
-        rdm_inv_queries.push(local_accounts.to_string());
+        get_local_accounts_inv();
     }
 
     let logical_drives = "Logical Drives";
     let logical_drives_idx = query_string.find("Logical Drives");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_local_drives_inv();
     }
 
     let logical_drives = "Network Adapters";
     let logical_drives_idx = query_string.find("Network Adapters");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_network_adapters_inv();
     }
 
     let logical_drives = "Printers";
     let logical_drives_idx = query_string.find("Printers");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_printers_inv();
     }
 
     let logical_drives = "Products";
     let logical_drives_idx = query_string.find("Products");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_services_inv();
     }
 
     let logical_drives = "Services";
     let logical_drives_idx = query_string.find("Services");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_products_inv();
     }
 
     let logical_drives = "Shares";
     let logical_drives_idx = query_string.find("Shares");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_shares_inv();
     }
 
     let logical_drives = "Start Up";
     let logical_drives_idx = query_string.find("Start Up");
     if let Some(_i) = logical_drives_idx {
-        rdm_inv_queries.push(logical_drives.to_string());
+        get_startup_inv();
     }
 
     let logical_drives = "System Information";
@@ -266,9 +159,6 @@ pub fn execute_inventory_query(query: &str) -> Vec<String> {
     let logical_drives = "Windows HotFixes";
     let logical_drives_idx = query_string.find("Windows HotFixes");
     if let Some(_i) = logical_drives_idx {
-        println!("{:?}", rdm_inv_queries.push(logical_drives.to_string()));
-        //print_xml();
+        get_hotfixes_inv();
     }
-
-    rdm_inv_queries
 }
