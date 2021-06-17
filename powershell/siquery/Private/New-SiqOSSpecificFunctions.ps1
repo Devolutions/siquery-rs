7d8c4c1ee53470586368dc5fcac000fde284a35f
Taylor Marvin <Taylor@Marvin.email>
2021-06-17 01:01:16 -0700

    Removed debugging lines

--- powershell/siquery/Private/New-SiqOSSpecificFunctions.ps1
+++ powershell/siquery/Private/New-SiqOSSpecificFunctions.ps1
@@ -5,30 +5,24 @@ Function New-SiqOSSpecificFunctions {
 	Begin {
 		$GenericFunctionTemplate = "function Get-Siq{Table} {`n`t[CmdletBinding()]`n`tparam()`n`n`tGet-SiqTableJson '{TableName}'`n}"
 		$Tables = Get-SiqTableName
-		Write-Host "Found $($Tables.Count) Tables. Originally had $AllOsCompatCount"
 		$Tables2 = $tables | ForEach-Object { [PSCustomOBject]@{ TableName = $_ ; TableNoUnderScore = (ConvertTo-PascalCase $_.Replace('_', '')) } }
-		Write-Host "Found $($Tables2.Count) Tables2."
 		$PublicFolder = Get-Item "$PSScriptRoot\..\Public"
 		$PublicFunctions = Get-ChildItem "$PublicFolder\*.ps1"
-		Write-Host "Found $($PublicFunctions.Count) Public Functions."
 	}
 
 	Process {
 		$ExistingTables = Foreach ($Function in $PublicFunctions) {
 			if ($Function.BaseName -imatch 'Get-Siq(?<TableName>\w*)') { $Matches['TableName'] }
 		}
-		Write-Host "Found $($ExistingTables.Count) Existing Tables."
 		Foreach ($Table in $Tables2) {
 			if ($ExistingTables -inotcontains $Table.'TableNoUnderScore') {
 				$TableFunction = $GenericFunctionTemplate.Replace('{Table}', $Table.'TableNoUnderScore')
 				$TableFunction = $TableFunction.Replace('{TableName}', $Table.'TableName')
 
 				$FunctionName = "Get-Siq$($Table.'TableNoUnderScore')"
-				New-Item -Path "$PublicFolder\$FunctionName.ps1" -ItemType File -Value $TableFunction -Verbose
+				New-Item -Path "$PublicFolder\$FunctionName.ps1" -ItemType File -Value $TableFunction | Out-Null
 				. ([ScriptBlock]::Create($TableFunction))
-				Export-ModuleMember -Function $FunctionName -Alias '*' -Verbose
-			} else {
-				Write-Host "Module already contains a function for $($Table.'TableNoUnderScore')"
+				Export-ModuleMember -Function $FunctionName -Alias '*'
 			}
 		}
 	}
